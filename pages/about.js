cae6e05bd5c435eb91097f3bf05d82cf13177258
Kai-Itakura <itakai199969@gmail.com>
2023-01-31 23:22:37 +0900

    🔥 ディレクトリの変更

--- pages/about.js
+++ pages/about.js
@@ -2,8 +2,8 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
-import { Links } from './components/Links'
-import { Center } from './components/Center'
+import { Links } from '../components/Links'
+import { Center } from '../components/Center'
 
 const inter = Inter({ subsets: ['latin'] })
 
