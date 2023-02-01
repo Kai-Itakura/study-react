1c2f50728d82e92aa63ae9a0d7efcabc6d5a2411
Kai-Itakura <itakai199969@gmail.com>
2023-02-02 01:04:50 +0900

    💄 index.jsxの背景色を変更

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -16,7 +16,7 @@ export default function Home() {
   }, []);
 
   useEffect(() => {
-    document.body.style.backgroundColor = 'pink';
+    document.body.style.backgroundColor = 'orange';
     console.log('マウント時');
 
     return () => {
