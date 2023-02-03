f618ecfdeba066cb522f98f724b4bba1f59dcd16
Kai-Itakura <itakai199969@gmail.com>
2023-02-03 16:42:15 +0900

    💄 buttonの配置を変更

--- src/components/Headline/index.jsx
+++ src/components/Headline/index.jsx
@@ -1,13 +1,17 @@
 import styles from './Headline.module.css'
 import { Vercel } from 'src/components/Vercel';
 
-export function Headline({ items }) {
+export function Headline({ items, handleReduce, handleIncrease }) {
     return (
-        <div className={styles.description}>
-            <p>
-                アイテムの数は&nbsp;{items.length}個&nbsp;です
-            </p>
-            <Vercel />
-        </div >
+        <>
+            <div className={styles.description}>
+                <p>
+                    アイテムの数は&nbsp;{items.length}個&nbsp;です
+                </p>
+                <Vercel />
+            </div >
+            <button onClick={handleReduce}>減らす</button>
+            <button onClick={handleIncrease}>増やす</button>
+        </>
     );
 }
--- src/components/Links/index.jsx
+++ src/components/Links/index.jsx
@@ -3,21 +3,17 @@ import styles from './Links.module.css'
 
 const inter = Inter({ subsets: ['latin'] });
 
-export function Links({ items, handleReduce, handleIncrease }) {
+export function Links({ items }) {
     return (
-        <>
-            <button onClick={handleReduce}>減らす</button>
-            <button onClick={handleIncrease}>増やす</button>
-            <div className={styles.grid}>
-                {items.map((item, index) => {
-                    return (
-                        <a key={index} href={item.href} className={styles.card} target="_blank" rel="noopener noreferrer">
-                            <h2 className={inter.className} dangerouslySetInnerHTML={{ __html: item.title }}></h2>
-                            <p className={styles.description}>{item.description}</p>
-                        </a>
-                    )
-                })}
-            </div>
-        </>
+        <div className={styles.grid}>
+            {items.map((item, index) => {
+                return (
+                    <a key={index} href={item.href} className={styles.card} target="_blank" rel="noopener noreferrer">
+                        <h2 className={inter.className} dangerouslySetInnerHTML={{ __html: item.title }}></h2>
+                        <p className={styles.description}>{item.description}</p>
+                    </a>
+                )
+            })}
+        </div>
     )
 }
